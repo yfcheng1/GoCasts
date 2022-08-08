242dffec68a73de5e2caa97e78934215b44d9faf
yfcheng1 <b99501048@gmail.com>
2022-08-07 21:09:11 -0400

    finished newDeck func

--- cards/deck.go
+++ cards/deck.go
@@ -5,8 +5,22 @@ import "fmt"
 // create a new type of 'deck', which is a slice of strings (like a class)
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // receiver type deck (d deck)
-// d: actual copy of the deck
+// d: receiver, actual copy of the deck => think about it as "this"
 // deck: every variable inside deck get access to the print method
 func (d deck) print() {
 	for i, card := range d {
