aefaf75aba3bfd9afd0268a8fb0db638df2f8eaf
yfcheng1 <b99501048@gmail.com>
2022-08-16 15:26:17 -0400

    finish all tests in cards

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // func to test the new deck fn
 func TestNewDeck(t *testing.T) {
@@ -13,10 +16,25 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
 
-	if d[len(d) - 1] != "King of Clubs" {
-		t.Errorf("Expected first card of King of Clubs, but got %v", d[len(d) - 1])
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected first card of King of Clubs, but got %v", d[len(d)-1])
 	}
 }
 
 // func test save and pull from file fn
 
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// delete file in dir
+	os.Remove("_decktesting")
+	// create a deck and save to file
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	// load from file
+	loadedDeck := newDeckFromFile("_decktesting")
+	// start compare
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length from file of 52, but got %v", len(loadedDeck))
+	}
+	// delete file in dir again
+	os.Remove("_decktesting")
+}
